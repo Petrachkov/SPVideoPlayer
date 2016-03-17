c2d0a14113eaf617db652a22aa84ede48041e6a9
sergey petrachkov <petrachkovsergey@gmail.com>
2016-03-17 12:25:25 +0700

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by sergey petrachkov on 17/03/16.
+//  Copyright © 2016 sergey petrachkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
