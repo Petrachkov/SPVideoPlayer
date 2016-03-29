d70b752258ef4a868ecb111cbc0be4efa074605d
sergey petrachkov <petrachkovsergey@gmail.com>
2016-03-29 15:32:07 +0700

    removed extra code from view controller

--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -10,16 +10,7 @@ import UIKit
 import AVKit
 import MediaPlayer
 
-class ViewController: UIViewController, MediaToolBarDelegate {
-
-	var player : AVPlayer? = nil
-	var playerLayer : AVPlayerLayer? = nil
-	var asset : AVAsset? = nil
-	var playerItem: AVPlayerItem? = nil
-	var toolbar : MediaToolBar!;
-	
-	
-	
+class ViewController: UIViewController {
 	var button : UIButton = UIButton(type: .Custom);
 	override func viewDidLoad() {
 		super.viewDidLoad();
@@ -35,40 +26,5 @@ class ViewController: UIViewController, MediaToolBarDelegate {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-	
-	func playTapped(toolBar: MediaToolBar) {
-
-		if(!toolBar.playing){
-			player!.play()
-		}
-		else{
-			player!.pause();
-		}
-	}
-	func dragDidEnd(slider: UISlider) {
-		player?.pause();
-		let seconds : Int64 = Int64(slider.value);
-		let preferredTimeScale : Int32 = 1;
-		let seekTime : CMTime = CMTimeMake(seconds, preferredTimeScale);
-		player?.seekToTime(seekTime);
-		player?.play();
-	}
-	
-	func stringFromTimeInterval(interval:NSTimeInterval) -> NSString {
-		
-		let ti = NSInteger(interval)
-		
-		let seconds = ti % 60
-		let minutes = (ti / 60) % 60
-		let hours = (ti / 3600)
-		
-		if (hours > 0){
-			return NSString(format: "%0.2d:%0.2d:%0.2d",hours,minutes,seconds)
-		}
-		else{
-			return NSString(format: "%0.2d:%0.2d",minutes,seconds)
-		}
-	}
-
 }
 
